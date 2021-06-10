ae8b8c761ec6b16672e34df4cc8629552fb9b63b
Shota Shimazu <hornet.live.mf@gmail.com>
2021-06-11 00:22:10 +0900

    revise

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,8 +1,27 @@
 package protocol
 
 type Protocol interface {
-	Ping()
-	Store()
-	FindNode()
-	FindValue()
+	Ping(p *KProtocol)
+	Store(p *KProtocol)
+	FindNode(p *KProtocol)
+	FindValue(p *KProtocol)
+}
+
+type KProtocol struct {
+}
+
+func NewProtocol() Protocol {
+	return &KProtocol{}
+}
+
+func Ping(p *KProtocol) {
+}
+
+func Store(p *KProtocol) {
+}
+
+func FindNode(p *KProtocol) {
+}
+
+func FindValue(p *KProtocol) {
 }
